24f6d8e79e544d2bf4902649972a57f39c910356
erlanggajatikusuma <erlanggajatikusuma007@gmail.com>
2020-12-13 21:03:27 +0700

    button navigation

--- App.js
+++ App.js
@@ -1,5 +1,10 @@
 import React from 'react';
-import {View, StyleSheet, Text} from 'react-native';
+import {View, StyleSheet, Text, Image} from 'react-native';
+import iconHome from './src/assets/icon/home-active.png';
+import iconOrder from './src/assets/icon/order.png';
+import iconHelp from './src/assets/icon/help.png';
+import iconInbox from './src/assets/icon/inbox.png';
+import iconAccount from './src/assets/icon/account.png';
 
 const App = () => {
   return (
@@ -7,50 +12,35 @@ const App = () => {
       {/* <Text style={styles.welcome}>Clone Gojek App</Text> */}
       <View style={styles.content} />
       <View style={styles.bottom}>
-        <View
-          style={{
-            backgroundColor: 'red',
-            flex: 1,
-            alignItems: 'center',
-            justifyContent: 'center',
-          }}>
-          <Text>Home</Text>
+        <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+          <Image style={{width: 26, height: 26}} source={iconHome} />
+          <Text style={{fontSize: 10, marginTop: 4, color: '#43AB4A'}}>
+            Home
+          </Text>
         </View>
-        <View
-          style={{
-            backgroundColor: 'green',
-            flex: 1,
-            alignItems: 'center',
-            justifyContent: 'center',
-          }}>
-          <Text>Order</Text>
+        <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+          <Image style={{width: 26, height: 26}} source={iconOrder} />
+          <Text style={{fontSize: 10, color: '#545454', marginTop: 4}}>
+            Order
+          </Text>
         </View>
-        <View
-          style={{
-            backgroundColor: 'pink',
-            flex: 1,
-            alignItems: 'center',
-            justifyContent: 'center',
-          }}>
-          <Text>Help</Text>
+        <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+          <Image style={{width: 26, height: 26}} source={iconHelp} />
+          <Text style={{fontSize: 10, color: '#545454', marginTop: 4}}>
+            Help
+          </Text>
         </View>
-        <View
-          style={{
-            backgroundColor: 'orange',
-            flex: 1,
-            alignItems: 'center',
-            justifyContent: 'center',
-          }}>
-          <Text>Inbox</Text>
+        <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+          <Image style={{width: 26, height: 26}} source={iconInbox} />
+          <Text style={{fontSize: 10, color: '#545454', marginTop: 4}}>
+            Inbox
+          </Text>
         </View>
-        <View
-          style={{
-            backgroundColor: 'purple',
-            flex: 1,
-            alignItems: 'center',
-            justifyContent: 'center',
-          }}>
-          <Text>Account</Text>
+        <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+          <Image style={{width: 26, height: 26}} source={iconAccount} />
+          <Text style={{fontSize: 10, color: '#545454', marginTop: 4}}>
+            Account
+          </Text>
         </View>
       </View>
     </View>
@@ -60,7 +50,7 @@ const App = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#F5FCFF',
+    backgroundColor: 'white',
   },
   content: {
     flex: 1,
@@ -68,7 +58,6 @@ const styles = StyleSheet.create({
   },
   bottom: {
     height: 54,
-    backgroundColor: 'yellow',
     flexDirection: 'row',
   },
 });
