ec1e6fc64ad058a1f4189b6d36d7e1da2fd80715
erlanggajatikusuma <erlanggajatikusuma007@gmail.com>
2020-12-14 22:42:11 +0700

    add search bar

--- App.js
+++ App.js
@@ -1,16 +1,49 @@
 import React from 'react';
-import {View, StyleSheet, Text, Image} from 'react-native';
+import {View, StyleSheet, Text, Image, TextInput} from 'react-native';
 import iconHome from './src/assets/icon/home-active.png';
 import iconOrder from './src/assets/icon/order.png';
 import iconHelp from './src/assets/icon/help.png';
 import iconInbox from './src/assets/icon/inbox.png';
 import iconAccount from './src/assets/icon/account.png';
+import iconSearch from './src/assets/icon/search.png';
+import iconPromo from './src/assets/icon/promo.png';
 
 const App = () => {
   return (
     <View style={styles.container}>
       {/* <Text style={styles.welcome}>Clone Gojek App</Text> */}
-      <View style={styles.content} />
+      <View style={styles.content}>
+        <View style={{marginHorizontal: 17, flexDirection: 'row'}}>
+          <View style={{position: 'relative', flex: 1}}>
+            <TextInput
+              placeholder="What do you want to eat ?"
+              style={{
+                paddingLeft: 45,
+                paddingRight: 20,
+                borderWidth: 1,
+                borderColor: '#E8E8E8',
+                borderRadius: 25,
+                height: 40,
+                fontSize: 13,
+                backgroundColor: 'white',
+                marginRight: 18,
+              }}
+            />
+            <Image
+              source={iconSearch}
+              style={{position: 'absolute', top: 5, left: 12}}
+            />
+          </View>
+          <View
+            style={{
+              width: 35,
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}>
+            <Image source={iconPromo} />
+          </View>
+        </View>
+      </View>
       <View style={styles.bottom}>
         <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
           <Image style={{width: 26, height: 26}} source={iconHome} />
@@ -50,11 +83,11 @@ const App = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'white',
   },
   content: {
     flex: 1,
-    backgroundColor: 'blue',
+    backgroundColor: 'white',
+    paddingTop: 15,
   },
   bottom: {
     height: 54,
