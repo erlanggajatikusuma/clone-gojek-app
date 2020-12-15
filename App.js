e2bd102b0171dc0a2367969ac7878df86e2401d7
erlanggajatikusuma <erlanggajatikusuma007@gmail.com>
2020-12-15 13:12:43 +0700

    add internal information section

--- App.js
+++ App.js
@@ -366,25 +366,25 @@ const App = () => {
                 backgroundColor: 'black',
                 opacity: 0.2,
                 borderRadius: 6,
+              }}
+            />
+            <View
+              style={{
+                height: 15,
+                width: 60,
+                position: 'absolute',
+                top: 16,
+                left: 16,
               }}>
-              <View
+              <Image
+                source={require('./src/assets/logo/white.png')}
                 style={{
-                  height: 15,
-                  width: 60,
-                  position: 'absolute',
-                  top: 16,
-                  left: 16,
-                }}>
-                <Image
-                  source={require('./src/assets/logo/white.png')}
-                  style={{
-                    width: undefined,
-                    height: undefined,
-                    resizeMode: 'contain',
-                    flex: 1,
-                  }}
-                />
-              </View>
+                  width: undefined,
+                  height: undefined,
+                  resizeMode: 'contain',
+                  flex: 1,
+                }}
+              />
             </View>
           </View>
           <View
@@ -393,7 +393,6 @@ const App = () => {
               paddingBottom: 20,
               borderBottomColor: '#E8E9ED',
               borderBottomWidth: 1,
-              marginBottom: 20,
             }}>
             <Text style={{fontSize: 16, fontWeight: 'bold', color: '#1C1C1C'}}>
               GO-NEWS
@@ -426,6 +425,170 @@ const App = () => {
               </Text>
             </TouchableOpacity>
           </View>
+        </View>
+        {/* Internal Information Section */}
+        <View style={{padding: 16, paddingBottom: 0}}>
+          <View style={{height: 15, width: 60, marginLeft: -4}}>
+            <Image
+              source={require('./src/assets/logo/gojek.png')}
+              style={{
+                width: undefined,
+                height: undefined,
+                resizeMode: 'contain',
+                flex: 1,
+              }}
+            />
+          </View>
+          <Text
+            style={{
+              marginTop: 15,
+              marginBottom: 20,
+              fontSize: 17,
+              fontWeight: 'bold',
+              color: '#1C1C1C',
+            }}>
+            Complete Your Profile
+          </Text>
+          <View style={{flexDirection: 'row', marginBottom: 16}}>
+            <View>
+              <Image
+                source={require('./src/assets/dummy/facebook-connect.png')}
+              />
+            </View>
+            <View style={{marginLeft: 16, flex: 1}}>
+              <Text
+                style={{fontSize: 15, fontWeight: 'bold', color: '#4A4A4A'}}>
+                Connect with Facebook
+              </Text>
+              <Text
+                style={{
+                  fontSize: 15,
+                  fontWeight: 'normal',
+                  color: '#4A4A4A',
+                  width: '70%',
+                }}>
+                Login faster without verification code
+              </Text>
+            </View>
+          </View>
+          <TouchableOpacity
+            style={{
+              backgroundColor: '#61A756',
+              paddingHorizontal: 12,
+              paddingVertical: 11,
+              alignSelf: 'flex-end',
+              borderRadius: 4,
+            }}>
+            <Text
+              style={{
+                fontSize: 13,
+                fontWeight: 'bold',
+                color: 'white',
+                textAlign: 'center',
+              }}>
+              CONNECT
+            </Text>
+          </TouchableOpacity>
+          <View
+            style={{
+              borderBottomColor: '#E8E9ED',
+              borderBottomWidth: 1,
+              marginTop: 16,
+            }}
+          />
+        </View>
+        {/* Go Food Banner Section */}
+        <View style={{padding: 16}}>
+          <View style={{position: 'relative'}}>
+            <Image
+              source={require('./src/assets/dummy/food-banner.jpg')}
+              style={{height: 170, width: '100%', borderRadius: 6}}
+            />
+            <View
+              style={{
+                width: '100%',
+                height: '100%',
+                position: 'absolute',
+                top: 0,
+                left: 0,
+                backgroundColor: 'black',
+                opacity: 0.15,
+                borderRadius: 6,
+              }}
+            />
+            <View
+              style={{
+                height: 15,
+                width: 60,
+                position: 'absolute',
+                top: 16,
+                left: 16,
+              }}>
+              <Image
+                source={require('./src/assets/logo/white.png')}
+                style={{
+                  width: undefined,
+                  height: undefined,
+                  resizeMode: 'contain',
+                  flex: 1,
+                }}
+              />
+            </View>
+            <View
+              style={{
+                position: 'absolute',
+                bottom: 0,
+                left: 0,
+                width: '100%',
+                flexDirection: 'row',
+                alignItems: 'center',
+                paddingHorizontal: 16,
+                paddingBottom: 16,
+              }}>
+              <View>
+                <Text
+                  style={{
+                    fontSize: 18,
+                    fontWeight: 'bold',
+                    color: 'white',
+                    marginBottom: 8,
+                  }}>
+                  Free GO-FOOD Voucher
+                </Text>
+                <Text style={{fontSize: 12, fontWeight: '400', color: 'white'}}>
+                  Quick, before they run out!
+                </Text>
+              </View>
+              <View style={{flex: 1, paddingLeft: 12}}>
+                <TouchableOpacity
+                  style={{
+                    backgroundColor: '#61A756',
+                    paddingHorizontal: 12,
+                    paddingVertical: 11,
+                    alignSelf: 'stretch',
+                    borderRadius: 4,
+                  }}>
+                  <Text
+                    style={{
+                      fontSize: 10,
+                      fontWeight: 'bold',
+                      color: 'white',
+                      textAlign: 'center',
+                    }}>
+                    GET VOUCHER
+                  </Text>
+                </TouchableOpacity>
+              </View>
+            </View>
+          </View>
+          <View
+            style={{
+              borderBottomColor: '#E8E9ED',
+              borderBottomWidth: 1,
+              marginTop: 16,
+              marginBottom: 20,
+            }}
+          />
         </View>
       </ScrollView>
       <View style={styles.bottom}>
