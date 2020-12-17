2d1ce71d8f616735837dc58aa633906198ba650f
erlanggajatikusuma <erlanggajatikusuma007@gmail.com>
2020-12-17 12:13:43 +0700

    4. install react-native camera

--- src/containers/pages/ScanQRCode/index.js
+++ src/containers/pages/ScanQRCode/index.js
@@ -1,6 +1,7 @@
 /* eslint-disable react-native/no-inline-styles */
-import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import React, {Component} from 'react';
+import {StyleSheet, Text, View, TouchableOpacity} from 'react-native';
+import {RNCamera} from 'react-native-camera';
 
 const IconWithText = (props) => {
   return (
@@ -33,92 +34,157 @@ const IconAction = () => {
   );
 };
 
-const ScanQRCode = () => {
-  return (
-    <View style={{flex: 1}}>
-      <View style={{flex: 1, backgroundColor: 'grey'}}>
+class ScanQRCode extends React.Component {
+  //   takePicture = async () => {
+  //     if (this.camera) {
+  //       const options = {quality: 0.5, base64: true};
+  //       const data = await this.camera.takePictureAsync(options);
+  //       console.log(data.uri);
+  //     }
+  //   };
+  render() {
+    return (
+      <View style={{flex: 1}}>
+        <View style={{flex: 1, backgroundColor: 'grey'}}>
+          <RNCamera
+            ref={(ref) => {
+              this.camera = ref;
+            }}
+            style={styles.preview}
+            type={RNCamera.Constants.Type.back}
+            flashMode={RNCamera.Constants.FlashMode.on}
+            androidCameraPermissionOptions={{
+              title: 'Permission to use camera',
+              message: 'We need your permission to use your camera',
+              buttonPositive: 'Ok',
+              buttonNegative: 'Cancel',
+            }}
+            androidRecordAudioPermissionOptions={{
+              title: 'Permission to use audio recording',
+              message: 'We need your permission to use your audio',
+              buttonPositive: 'Ok',
+              buttonNegative: 'Cancel',
+            }}
+            onGoogleVisionBarcodesDetected={({barcodes}) => {
+              console.log(barcodes);
+            }}
+          />
+          <View
+            style={{
+              flexDirection: 'row',
+              paddingHorizontal: 16,
+              justifyContent: 'space-between',
+              marginTop: 16,
+              position: 'absolute',
+              top: 0,
+              left: 0,
+              width: '100%',
+            }}>
+            <IconAction />
+            <View
+              style={{
+                flexDirection: 'row',
+                justifyContent: 'space-between',
+                width: 80,
+              }}>
+              <IconAction />
+              <IconAction />
+            </View>
+            {/* <View
+              style={{flex: 0, flexDirection: 'row', justifyContent: 'center'}}>
+              <TouchableOpacity
+                onPress={this.takePicture.bind(this)}
+                style={styles.capture}>
+                <Text style={{fontSize: 14}}> SNAP </Text>
+              </TouchableOpacity>
+            </View> */}
+          </View>
+        </View>
         <View
           style={{
-            flexDirection: 'row',
+            height: 200,
+            backgroundColor: 'white',
             paddingHorizontal: 16,
-            justifyContent: 'space-between',
-            marginTop: 16,
           }}>
-          <IconAction />
+          <View style={{alignItems: 'center', marginTop: 8, marginBottom: 18}}>
+            <View
+              style={{
+                width: 40,
+                height: 3,
+                backgroundColor: '#E0E0E0',
+                marginVertical: 1,
+              }}
+            />
+            <View
+              style={{
+                width: 40,
+                height: 3,
+                backgroundColor: '#E0E0E0',
+                marginVertical: 1,
+              }}
+            />
+          </View>
           <View
             style={{
               flexDirection: 'row',
               justifyContent: 'space-between',
-              width: 80,
+              alignItems: 'center',
             }}>
-            <IconAction />
-            <IconAction />
+            <Text style={{fontSize: 20, fontWeight: 'bold', color: 'black'}}>
+              More Options
+            </Text>
+            <Text style={{fontSize: 14, color: '#61A756', fontWeight: 'bold'}}>
+              Shortcut
+            </Text>
           </View>
-        </View>
-      </View>
-      <View
-        style={{height: 200, backgroundColor: 'white', paddingHorizontal: 16}}>
-        <View style={{alignItems: 'center', marginTop: 8, marginBottom: 18}}>
-          <View
-            style={{
-              width: 40,
-              height: 3,
-              backgroundColor: '#E0E0E0',
-              marginVertical: 1,
-            }}
-          />
-          <View
-            style={{
-              width: 40,
-              height: 3,
-              backgroundColor: '#E0E0E0',
-              marginVertical: 1,
-            }}
-          />
-        </View>
-        <View
-          style={{
-            flexDirection: 'row',
-            justifyContent: 'space-between',
-            alignItems: 'center',
-          }}>
-          <Text style={{fontSize: 20, fontWeight: 'bold', color: 'black'}}>
-            More Options
-          </Text>
-          <Text style={{fontSize: 14, color: '#61A756', fontWeight: 'bold'}}>
-            Shortcut
-          </Text>
-        </View>
-        <View
-          style={{
-            flexDirection: 'row',
-            alignItems: 'flex-start',
-            marginTop: 14,
-            width: '100%',
-          }}>
           <View
             style={{
               flexDirection: 'row',
               alignItems: 'flex-start',
-              width: 160,
-              justifyContent: 'space-between',
-              paddingRight: 12,
+              marginTop: 14,
+              width: '100%',
             }}>
-            <IconWithText title="Phone Number" />
-            <IconWithText title="GoPay Code" />
-          </View>
-          <View style={{width: 1, height: 60, backgroundColor: 'grey'}} />
-          <View style={{flex: 1, paddingLeft: 12}}>
-            <Text>
-              Your recent GoPay receivers will show here. No admin fees!
-            </Text>
+            <View
+              style={{
+                flexDirection: 'row',
+                alignItems: 'flex-start',
+                width: 160,
+                justifyContent: 'space-between',
+                paddingRight: 12,
+              }}>
+              <IconWithText title="Phone Number" />
+              <IconWithText title="GoPay Code" />
+            </View>
+            <View style={{width: 1, height: 60, backgroundColor: 'grey'}} />
+            <View style={{flex: 1, paddingLeft: 12}}>
+              <Text>
+                Your recent GoPay receivers will show here. No admin fees!
+              </Text>
+            </View>
           </View>
         </View>
       </View>
-    </View>
-  );
-};
+    );
+  }
+}
 
 export default ScanQRCode;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  preview: {
+    width: '100%',
+    height: '100%',
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  capture: {
+    flex: 0,
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    padding: 15,
+    paddingHorizontal: 20,
+    alignSelf: 'center',
+    margin: 20,
+  },
+});
